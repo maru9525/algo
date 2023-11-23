b75cdb60be0eae81d4efb22259e607884b55dcdb
CHOI JEONGHUN <68151945+maru9525@users.noreply.github.com>
2023-11-23 19:35:23 +0900

    [level 2] Title: 가장 큰 수, Time: 2.58 ms, Memory: 65.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        // System.out.print(numbers.length);
+        String[] s = new String[numbers.length];
+        for(int i=0; i<s.length; i++){
+            s[i] = Integer.toString(numbers[i]);
+            // System.out.print(s[i]);
+        }
+        Arrays.sort(s, new Comparator<String>(){
+            @Override
+            public int compare(String o1, String o2){
+                // System.out.println("o1 : " + o1+o2 + " o2 : " + o2+o1);
+                // System.out.println("answer : " + (o2+o1).compareTo(o1+o2));
+                return ((o2+o1).compareTo(o1+o2));
+            }
+        });
+        if(s[0].equals("0")) return "0";
+        for(String str : s) answer += str;
+
+        return answer;
+    }
+}
