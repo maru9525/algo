c4002353833e04cceb29c9ac24ca335479cce9f6
CHOI JEONGHUN <68151945+maru9525@users.noreply.github.com>
2023-12-30 16:21:42 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,10 @@
+SELECT '/home/grep/src/' || B.BOARD_ID || '/' || B.FILE_ID || B.FILE_NAME || B.FILE_EXT AS FILE_PATH
+FROM (SELECT *
+        FROM (SELECT *
+                FROM USED_GOODS_BOARD
+                ORDER BY VIEWS DESC
+             )
+WHERE ROWNUM  = 1
+      ) A, USED_GOODS_FILE B
+WHERE A.BOARD_ID = B.BOARD_ID
+ORDER BY B.FILE_ID DESC;
