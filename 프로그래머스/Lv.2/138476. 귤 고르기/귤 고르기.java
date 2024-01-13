8b45a57b4663ecb0da9c46021be77fd907e51bcf
CHOI JEONGHUN <68151945+maru9525@users.noreply.github.com>
2024-01-13 16:18:52 +0900

    [level Lv.2] Title: 귤 고르기, Time: 43.19 ms, Memory: 99.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<tangerine.length; i++) {
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>(map.values());
+        Collections.sort(list, Collections.reverseOrder());
+        
+        for(int temp : list) {
+            if(sum + temp >= k) {
+                answer++;
+                break;
+            }else {
+                sum += temp;
+                answer++;
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
