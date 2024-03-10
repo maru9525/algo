7ae39c528cb33e651e7d6eb82872546648c830da
CHOI JEONGHUN <68151945+maru9525@users.noreply.github.com>
2024-03-10 17:16:50 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        int days = 10;
+        int len = discount.length;
+        // System.out.print(len);
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<want.length; i++) {
+            map.put(want[i], number[i]);
+        }
+        outbreak:
+        for(int i = 0; i < discount.length - days + 1; i++){
+            Map<String, Integer> dMap = new HashMap<>();
+            
+            for(int j = 0; j < days; j++){
+                dMap.put(discount[i + j], dMap.getOrDefault(discount[i + j], 0) + 1);
+            }
+            
+            Boolean isIdentical = true;
+            
+            for(String key : map.keySet()){
+                if(map.get(key) != dMap.get(key)){
+                    isIdentical = false;
+                    break;
+                }
+            }
+            
+            answer += isIdentical ? 1 : 0;
+        }
+        return answer;
+    }
+}
