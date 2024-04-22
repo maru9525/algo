c32077b5fd5c7b2a9b6329c7a169b7b595f47199
CHOI JEONGHUN <68151945+maru9525@users.noreply.github.com>
2024-04-22 21:50:01 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 12.41 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,59 +1,56 @@
 import java.util.*;
 class Solution {
-    static int[][] arr;
+    static int[][] graph;
+
     
-    static int cnt;
   
     public int solution(int n, int[][] wires) {
-       int answer = n;
-        arr= new int[n+1][n+1];
-        
-        //1. 인접행렬에 input
-        for(int i=0; i<wires.length; i++){
-            arr[wires[i][0]][wires[i][1]]=1;
-            arr[wires[i][1]][wires[i][0]]=1;
+        int answer = n;
+         graph = new int[n+1][n+1];
+        //연결
+        for(int i=0; i<wires.length; i++) {
+            int a = wires[i][0];
+            int b = wires[i][1];
+            graph[a][b] = graph[b][a] = 1;
         }
         
-        //2. 선을 하나씩 끊어보며 순회
-        int a, b;
-        for(int i=0; i<wires.length; i++){
-            a= wires[i][0];
-            b= wires[i][1];
+        for(int i=0; i<wires.length; i++) {
+            int a = wires[i][0];
+            int b = wires[i][1];
+            //선 끊기
+            graph[a][b] = graph[b][a] = 0;
             
-            //선을 하나 끊고
-            arr[a][b]=0;
-            arr[b][a]=0;
+            answer = Math.min(answer, bfs(n, a));
+            //선 연결
+            graph[a][b] = graph[b][a] = 1;
             
-            //bfs
-            answer= Math.min(answer, bfs(n, a));
-            
-            //선 다시 복구
-            arr[a][b]=1;
-            arr[b][a]=1;
         }
+        
+        
         
         return answer;
     }
-    
-    public int bfs(int n, int start){
-        int[] visit= new int[n+1];
-        int cnt=1;
+    static int bfs(int n, int start) {
+        int[] visited = new int[n+1];
+        int cnt = 1;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
         
-        Queue<Integer> queue= new LinkedList<>();
-        queue.offer(start);
-        
-        while(!queue.isEmpty()){
-            int point= queue.poll();
-            visit[point]= 1;
+        while(!q.isEmpty()) {
+            int p = q.poll();
+            visited[p] = 1;
             
-            for(int i=1; i<=n; i++){ //point와 연결된 애들 중에 방문한적 없는 노드 전부 큐에 넣기
-                if(visit[i]==1) continue;
-                if(arr[point][i]==1) {
-                    queue.offer(i);
+            for(int i = 1; i<=n; i++) {
+                if(visited[i] == 1) continue;
+                if(graph[p][i] == 1) {
+                    q.offer(i);
                     cnt++;
                 }
             }
         }
-        return (int)Math.abs(n-2*cnt); //cnt-(n-cnt);
-    }//bfs
+        return (int)Math.abs(n-2*cnt);
+        
+    }
+    
+    
 }
