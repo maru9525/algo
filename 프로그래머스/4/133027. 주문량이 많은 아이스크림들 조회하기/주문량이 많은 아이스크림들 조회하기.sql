c36fb29b3aacb30ae7c97f85ef228c8b1a72cc11
CHOI JEONGHUN <68151945+maru9525@users.noreply.github.com>
2024-11-01 22:05:00 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,16 +1,32 @@
--- 코드를 입력하세요
--- 7월 아이스크림 총 주문량 + 
--- 상반기의 아이스크림 총 주문량 
--- 큰 순서대로 상위 3개
-select FLAVOR
-from (
-SELECT f.FLAVOR as FLAVOR, sum(f.TOTAL_ORDER) + sum(j.TOTAL_ORDER)
-from FIRST_HALF f,  JULY j
-where 1 = 1
-and f.FLAVOR = j.FLAVOR
-group by f.FLAVOR
-order by sum(f.TOTAL_ORDER) + sum(j.TOTAL_ORDER) desc
-)
-where rownum <= 3
+-- 7월 아이스크림 총 주문량과 상반기의 아이스크림 총 주문량을 더한 값
+-- 상위 3개 맛 rownum
+-- FIRST_HALF, JULY / SHIPMENT_ID, TOTAL_ORDER
+SELECT * 
+  FROM (
+  
+    SELECT A.FLAVOR
+      FROM FIRST_HALF A , JULY B
+     WHERE 1 = 1
+       AND A.FLAVOR = B.FLAVOR
+     GROUP BY A.FLAVOR
+     ORDER BY SUM(A.TOTAL_ORDER + B.TOTAL_ORDER) DESC
+ )
+ WHERE ROWNUM <= 3
 
- -- limit 3
+-- WHERE ROWNUM <= 3
+    
+    
+    
+    
+    
+    
+
+  
+  
+-- SELECT FLAVOR, TOTAL_ORDER
+--   FROM FIRST_HALF
+  
+  -- SELECT FLAVOR, TOTAL_ORDER
+  -- FROM JULY 
+
+
