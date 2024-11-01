64386d8c90c71be71c82d34104e9271187c40109
CHOI JEONGHUN <68151945+maru9525@users.noreply.github.com>
2024-11-01 21:28:07 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -0,0 +1,9 @@
+-- 만원 단위의 가격대 별로 상품 개수
+-- PRICE_GROUP, PRODUCTS
+-- 가격대 정보는 각 구간의 최소금액(10,000원 이상 ~ 20,000 미만인 구간인 경우 10,000
+ -- 가격대를 기준으로 오름차순
+SELECT TRUNC(PRICE/10000) * 10000 AS PRICE_GROUP, COUNT(*) AS PRODUCTS
+  FROM PRODUCT
+ GROUP BY TRUNC(PRICE/10000) * 10000
+ ORDER BY PRICE_GROUP
+ 
